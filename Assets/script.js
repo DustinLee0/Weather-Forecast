abf5487be5683d023709c573db9f26226dfece97
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-10-23 16:32:07 -0400

    added bootstrap library

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,9 @@
+const searchInput = document.querySelector('#search-input')
+const searchBtn = document.querySelector('#search-btn')
+
+searchInput.addEventListener('submit', () => {console.log('hello world')})
+searchBtn.addEventListener('click', () => {console.log('hello world')})
+
 //  fetch latitude from city search
 const weatherKey = '84664db1c48cbb0a713cbe7d4c948b5b';
 const city = 'Toronto';
