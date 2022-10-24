a0e2c8d0c42c30caa3b35cd267eb9b2167eb79aa
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-10-24 15:07:23 -0400

    added local storage functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,61 @@
-const searchInput = document.querySelector('#search-input')
-const searchBtn = document.querySelector('#search-btn')
+const apiKey = '84664db1c48cbb0a713cbe7d4c948b5b';
+const searchForm = document.querySelector("#search-form");
+const localSave = [];
 
-searchInput.addEventListener('submit', () => {console.log('hello world')})
-searchBtn.addEventListener('click', () => {console.log('hello world')})
+searchForm.addEventListener('submit', saveInput);
 
-//  fetch latitude from city search
-const weatherKey = '84664db1c48cbb0a713cbe7d4c948b5b';
-const city = 'Toronto';
-const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey + '&units=metric';
+function saveInput(e) {
+    e.preventDefault();
+    let input = document.querySelector('#search-input');
+    let save = input.value
 
+    localSave.push(save)
+    localStorage.setItem('cityWeater', JSON.stringify(localSave))
 
-fetch(apiCall)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (response) {
-       console.log(response);
-        let latitude ;
-        let longitude;
-        
+    console.log(input.value);
+    getLatLong(input.value);
 
+}
 
+function getLatLong(input) {
+    //  fetch latitude from city search
+    const city = input;
+    const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
 
+    fetch(apiCall)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+        console.log(response);
+            let latitude = response.coord.lat;
+            let longitude = response.coord.lon;
 
+            getForecast(latitude, longitude);
 
-     })
-    .catch(function (err) {
-         console.error(err)
+            
+        })
+        .catch(function (err) {
+            console.error(err);
 
-    });
+        });
+}
 
+function getForecast(latitude, longitude) {
+    console.log(latitude);
+    console.log(longitude);
+}
     
+// fetch 5 day forecast data
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=43.7001&lon=-79.4163&appid=' + apiKey)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (response) {
+//        console.log(response);
 
-//  
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=43.7001&lon=-79.4163&appid=' + weatherKey)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (response) {
-       console.log(response)
+//      })
+//     .catch(function (err) {
+//          console.error(err);
 
-     })
-    .catch(function (err) {
-         console.error(err)
-
-    });
+//     });
