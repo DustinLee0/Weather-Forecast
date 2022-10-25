5c7b7799e524d2c3b8b6e3ba503b3b9d7e9631ac
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-10-25 17:10:26 -0400

    added current forecast card functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 const apiKey = '84664db1c48cbb0a713cbe7d4c948b5b';
 const searchForm = document.querySelector("#search-form");
+const listCard = document.querySelector('#saved-list')
+const weatherCard = document.querySelector('#weather-card')
 const localSave = [];
 
 searchForm.addEventListener('submit', saveInput);
@@ -7,18 +9,27 @@ searchForm.addEventListener('submit', saveInput);
 function saveInput(e) {
     e.preventDefault();
     let input = document.querySelector('#search-input');
-    let save = input.value
+    let savedInput = input.value;
 
-    localSave.push(save)
-    localStorage.setItem('cityWeater', JSON.stringify(localSave))
+    //save input string and push to an array to store
+    localSave.push(savedInput);
+    localStorage.setItem('cityWeather', JSON.stringify(localSave));
 
-    console.log(input.value);
+    savedCityList(savedInput);
     getLatLong(input.value);
 
 }
 
+// creates list item per city in local storage
+function savedCityList(city) {
+    const listEl = document.createElement('li');
+    listEl.innerHTML = city
+    listEl.classList.add('btn', 'btn-primary');
+    listCard.appendChild(listEl);
+}
+
+//  FIRST API CALL: fetch coordinates from city search
 function getLatLong(input) {
-    //  fetch latitude from city search
     const city = input;
     const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
 
@@ -27,12 +38,42 @@ function getLatLong(input) {
             return response.json();
         })
         .then(function (response) {
-        console.log(response);
-            let latitude = response.coord.lat;
-            let longitude = response.coord.lon;
+            console.log(response);
+            let cityName = response.name;
+            let cityIcon = `http://openweathermap.org/img/w/${response.weather[0].icon}.png`
+            let cityTemp = response.main.temp;
+            let cityWind = response.wind.speed * 3.6; // in m/s -> convert to km/hr
+            let cityHumidity = response.main.humidity;
+            let today = new Date().toLocaleDateString();
+            const newDiv = document.createElement('div');
+            const newH2 = document.createElement('h2');
+            const newIMG = document.createElement('img')
+            const newPTemp = document.createElement('p');
+            const newPWind = document.createElement('p');
+            const newPHumidex = document.createElement('p');
 
-            getForecast(latitude, longitude);
-
+            //  CURRENT FORECAST
+            newIMG.src = cityIcon ;
+            newIMG.classList.add('inline');
+            newH2.innerHTML = cityName + ' ' + today;
+            newH2.appendChild(newIMG);
+            newDiv.append(newH2);            
+            //  CURRENT FORECAST: CONTENT
+            newPTemp.innerHTML = 'Temperature: ' + cityTemp + ' °C';
+            newPWind.innerHTML = 'Wind: ' + cityWind.toFixed(2) + ' km/s';
+            newPHumidex.innerHTML = 'Humidity: ' + cityHumidity + '%';
+            newPTemp.classList.add('data-content')
+            newPWind.classList.add('data-content')
+            newPHumidex.classList.add('data-content')
+            newDiv.append(newPTemp, newPWind, newPHumidex);
+            newDiv.classList.add('current-forecast')
+            
+            weatherCard.append(newDiv)
+            
+            
+            // let latitude = response.coord.lat;
+            // let longitude = response.coord.lon;
+            // getForecast(latitude, longitude);
             
         })
         .catch(function (err) {
@@ -41,13 +82,28 @@ function getLatLong(input) {
         });
 }
 
+// SECOND API CALL:  5 day forecast data (SEARCH BY LONGITUDE AND LATITUDE)
 function getForecast(latitude, longitude) {
     console.log(latitude);
     console.log(longitude);
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+       console.log(response);
+
+     })
+    .catch(function (err) {
+         console.error(err);
+
+    });
 }
-    
-// fetch 5 day forecast data
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=43.7001&lon=-79.4163&appid=' + apiKey)
+
+
+// 5 DAY FORECAST BY SEARCHING BY CITY
+// fetch('https://api.openweathermap.org/data/2.5/forecast?q=washington&appid=' + apiKey)
 //     .then(function (response) {
 //         return response.json();
 //     })
