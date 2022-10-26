88b3cac36aa9807a264101ef5af4472ad04cc449
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-10-26 17:07:41 -0400

    added fetch request to get uvi data

--- Assets/script.js
+++ Assets/script.js
@@ -16,7 +16,8 @@ function saveInput(e) {
     localStorage.setItem('cityWeather', JSON.stringify(localSave));
 
     savedCityList(savedInput);
-    getLatLong(input.value);
+    getCurrentForecast(savedInput);
+    // get5DayForecast(savedInput);
 
 }
 
@@ -26,12 +27,14 @@ function savedCityList(city) {
     listEl.innerHTML = city
     listEl.classList.add('btn', 'btn-primary');
     listCard.appendChild(listEl);
+
+    //MAKE IF STATEMENT TO CHECK IF SAME CITY IS ENTERED, IT WOULD NOT MAKE ANOTHER DUPLICATE BUTTON
+
 }
 
-//  FIRST API CALL: fetch coordinates from city search
-function getLatLong(input) {
-    const city = input;
-    const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
+//  FIRST API CALL: fetch current forecast from city search
+function getCurrentForecast(input) {
+    const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=' + input + '&appid=' + apiKey + '&units=metric';
 
     fetch(apiCall)
         .then(function (response) {
@@ -40,11 +43,12 @@ function getLatLong(input) {
         .then(function (response) {
             console.log(response);
             let cityName = response.name;
-            let cityIcon = `http://openweathermap.org/img/w/${response.weather[0].icon}.png`
-            let cityTemp = response.main.temp;
-            let cityWind = response.wind.speed * 3.6; // in m/s -> convert to km/hr
-            let cityHumidity = response.main.humidity;
             let today = new Date().toLocaleDateString();
+            let cityIcon = `http://openweathermap.org/img/w/${response.weather[0].icon}.png`;
+            let cityTemp = response.main.temp;
+            let cityWind = response.wind.speed * 3.6; // in m/s -> convert to km/hr;
+            let cityHumidity = response.main.humidity;
+
             const newDiv = document.createElement('div');
             const newH2 = document.createElement('h2');
             const newIMG = document.createElement('img')
@@ -53,65 +57,115 @@ function getLatLong(input) {
             const newPHumidex = document.createElement('p');
 
             //  CURRENT FORECAST
-            newIMG.src = cityIcon ;
+            newIMG.src = cityIcon;
             newIMG.classList.add('inline');
-            newH2.innerHTML = cityName + ' ' + today;
+            newH2.innerHTML = cityName + ' ' + today + ' ';
             newH2.appendChild(newIMG);
-            newDiv.append(newH2);            
+            newDiv.append(newH2);
             //  CURRENT FORECAST: CONTENT
             newPTemp.innerHTML = 'Temperature: ' + cityTemp + ' °C';
-            newPWind.innerHTML = 'Wind: ' + cityWind.toFixed(2) + ' km/s';
+            newPWind.innerHTML = 'Wind: ' + cityWind.toFixed(2) + ' km/hr';
             newPHumidex.innerHTML = 'Humidity: ' + cityHumidity + '%';
-            newPTemp.classList.add('data-content')
-            newPWind.classList.add('data-content')
-            newPHumidex.classList.add('data-content')
+            newPTemp.classList.add('data-content');
+            newPWind.classList.add('data-content');
+            newPHumidex.classList.add('data-content');
             newDiv.append(newPTemp, newPWind, newPHumidex);
-            newDiv.classList.add('current-forecast')
-            
-            weatherCard.append(newDiv)
-            
-            
-            // let latitude = response.coord.lat;
-            // let longitude = response.coord.lon;
-            // getForecast(latitude, longitude);
-            
+            newDiv.classList.add('current-forecast');
+
+            weatherCard.append(newDiv);
+
+            oneCallAPI(response.coord.lat, response.coord.lon);
         })
         .catch(function (err) {
             console.error(err);
-
         });
 }
 
-// SECOND API CALL:  5 day forecast data (SEARCH BY LONGITUDE AND LATITUDE)
-function getForecast(latitude, longitude) {
-    console.log(latitude);
-    console.log(longitude);
+function oneCallAPI(latitude, longitude) {
+    let lat = latitude;
+    let long = longitude;
+    console.log(lat)
+    console.log(long)
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-       console.log(response);
+    fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + long + '&appid=' + apiKey + '&units=metric')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response)
+            let currentCard = document.querySelector('.current-forecast')
+            let uvIndex = response.current.uvi;
+            
+            const newPUVIndex = document.createElement('p');
 
-     })
-    .catch(function (err) {
-         console.error(err);
+            newPUVIndex.innerHTML = 'UV Index: ' + uvIndex;
+            newPUVIndex.classList.add('data-content');
+            currentCard.append(newPUVIndex);
 
-    });
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
+}
+
+// SECOND API CALL:  5 day forecast from city search
+function get5DayForecast(input) {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input + '&units=metric&appid=' + apiKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response);
+            let forecastDate = response.list[3].dt_txt;;
+            let forecastIcon = `http://openweathermap.org/img/w/${response.list[3].weather[0].icon}.png`;
+            let forecastTemp = response.list[3].main.temp;
+            let forecastWind = response.list[1].wind.speed;
+            let forecastHumidity = response.list[1].main.humidity;
+            console.log(forecastDate);
+            // console.log(forecastIcon);
+            // console.log(forecastTemp);
+            // console.log(forecastWind);
+            // console.log(forecastHumidity);
+            const newDiv = document.createElement('div');
+            const newH3 = document.createElement('h3');
+            const newIMG = document.createElement('img');
+            const newPTemp = document.createElement('p');
+            const newPWind = document.createElement('p');
+            const newPHumidex = document.createElement('p');
+
+            newIMG.src = forecastIcon;
+            newH3.innerHTML = forecastDate;
+            newPTemp.innerHTML = 'Temperature: ' + forecastTemp + ' °C';
+            newPWind.innerHTML = 'Wind: ' + forecastWind;
+            newPHumidex.innerHTML = 'Humidity: ' + forecastHumidity + '%';
+            newPTemp.classList.add('data-content');
+            newPWind.classList.add('data-content');
+            newPHumidex.classList.add('data-content');
+            newDiv.classList.add('forecast-card');
+            newDiv.append(newH3, newIMG, newPTemp, newPWind, newPHumidex);
+
+            weatherCard.append(newDiv);
+
+
+
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 }
 
 
-// 5 DAY FORECAST BY SEARCHING BY CITY
-// fetch('https://api.openweathermap.org/data/2.5/forecast?q=washington&appid=' + apiKey)
+// fetch('https://api.openweathermap.org/data/3.0/onecall?lat=43.7001&lon=-79.4163&appid=cebf833bd004e8474d29b759620ba9d3&units=metric')
 //     .then(function (response) {
 //         return response.json();
 //     })
 //     .then(function (response) {
-//        console.log(response);
+//         console.log(response)
 
-//      })
+
+
+//     })
 //     .catch(function (err) {
-//          console.error(err);
-
+//         console.error(err);
 //     });
